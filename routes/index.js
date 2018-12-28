b03b684ea186e8fadf37168dfd096cce91e0d539
Nathan Ryder <nathanryder16@gmail.com>
2018-12-28 15:48:13 +0000

    Added deploy script

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var exec = require("child_process").exec;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post("/deploy", function(req, res, next) {
+  exec("php deploy.php", function (error, stdout, stderr) {
+    res.send(stdout);
+  });
+});
+
 
 module.exports = router;
