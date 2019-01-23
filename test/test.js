9f530d4fb8eac1f9ac573bfcd0d531749ff05620
Nathan Ryder <nathanryder16@gmail.com>
2019-01-23 19:13:36 +0000

    Moved from supertest to chai

--- test/test.js
+++ test/test.js
@@ -1,11 +1,29 @@
-var request = require('supertest');
+//var request = require('supertest');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
 var app = require('../app.js');
 
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+/*
 describe('GET /', function() {
  it('respond with hello world', function(done) {
 
  //navigate to root and check the the response is "hello world"
- request(app).get('/test').expect('hello world test', done);
+ chai.request(app).get("/").
+ //request(app).get('/test').expect('hello world test', done);
  });
 });
+*/
 
+describe('GET /', () => {
+  it("respond with hello world", (done) => {
+    chai.request(app).get("/test")
+      .end((err, res) => {
+	res.text.should.be.eql('hello world test');
+        done();
+      });
+  });
+});
