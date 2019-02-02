ae70525a59b55af82c439bcfafef8b79a9f4291d
Nathan Ryder <nathanryder16@gmail.com>
2019-02-02 13:02:38 +0000

    Added some tests for user creation

--- models/database.js
+++ models/database.js
@@ -1,5 +1,20 @@
 var config = require("./config.js");
 var mongoose = require("mongoose");
-var connection = mongoose.connect("mongodb://" + config.username + ":" + config.password + "@" + config.host + ":" + config.port + "/" + config.database);
+var connection;
+
+const DB_URI = "mongodb://" + config.username + ":" + config.password + "@" + config.host + ":" + config.port + "/" + config.database;
+
+if (process.env.NODE_ENV === "test") {
+    var Mockgoose = require("mockgoose").Mockgoose;
+    var mockgoose = new Mockgoose(mongoose);
+
+    mockgoose.prepareStorage()
+        .then(() => {
+            connection = mongoose.connect(DB_URI);
+        });
+} else {
+    connection = mongoose.connect(DB_URI);
+}
+
 
 exports.connection = connection;
