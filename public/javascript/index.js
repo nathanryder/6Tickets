ba581b08e94f6181e3ac3a83738d0e8fa19bc92b
ertagon2 <ertagonn@gmail.com>
2019-04-04 22:23:29 +0200

    Improved the category slider

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -13,17 +13,27 @@ $(document).ready(function() {
             url: 'api/categories',
             dataType: 'json',
             success: function (res) {
-                gridDisplay(res)
+                gridDisplay(res);
+                updateSlider();
             }
         });
     }
 
 //    display categories on the page
     function gridDisplay(cat) {
-        var output="";
+        var output=
+            "<div class='swiper-slide'>" +
+            "    <div class='row no-gutters gutters-1'>";
 
         for(var i=0; i<cat.length; i++){
             var category= cat[i].name;
+            if(i%4===0 && i>0){
+                output+=
+                    "    </div>"+
+                    "</div>"+
+                    "<div class='swiper-slide'>" +
+                    "    <div class='row no-gutters gutters-1'>";
+            }
 
             output+=
                 "<div class='col-6 col-md-3 mb-1'>" +
@@ -33,41 +43,23 @@ $(document).ready(function() {
                 "            <h5>"+category+"</h5>" +
                 "        </div>" +
                 "   </a>" +
-                "</div>"
+                "</div>";
         }
+        output+=
+            "    </div>"+
+            "</div>";
         $("#categoryGrid").html(output);
     }
 
-//    More advanced
-    //    display categories on the page
-    // function gridDisplay(cat) {
-    //     var output=
-    //         "<div class='swiper-slide'>" +
-    //         "    <div class='row no-gutters gutters-1\'>";
-    //
-    //     for(var i=0; i<cat.length && i>8; i++){
-    //         var category= cat[i].name;
-    //         if(i==4) {
-    //             output+=
-    //                 "   </div>"+
-    //                 "</div>"+
-    //                 "<div class='swiper-slide'>";
-    //         }
-    //         output+=
-    //             "<div class='col-6 col-md-3 mb-1'>" +
-    //             "    <a href='/grid' class='card'>" +
-    //             "        <img src='images/categories/"+category+".jpeg' alt='' class='card-img-top'>" +
-    //             "        <div class='card-img-overlay card-img-overlay-bottom p-2 primary'>" +
-    //             "            <h5>"+category+"</h5>" +
-    //             "        </div>" +
-    //             "   </a>" +
-    //             "</div>";
-    //
-    //         if(i>3 && i==cat.length-1){
-    //             output+="</div>";
-    //         }
-    //     }
-    //     $("#categoryGrid").html(output);
-    // }
+    function updateSlider() {
+        var categoriesSlider = new Swiper('#categories-slider', {
+            navigation: {
+                prevEl: '#categories-slider-prev',
+                nextEl: '#categories-slider-next'
+            }
+        });
+        categoriesSlider.update();
+    }
+
 
 });
