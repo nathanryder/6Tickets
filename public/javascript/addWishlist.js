6040aac9835ea00c604977d33ee9e348a4f9fed6
Nathan Ryder <nathanryder16@gmail.com>
2019-04-07 20:29:57 +0100

    Added wishlist code

--- public/javascript/addWishlist.js
+++ public/javascript/addWishlist.js
@@ -1,16 +1,17 @@
-
-
 $(document).ready(function () {
 
     $(".wishlist").click(function () {
-        var ticketID = $(this).attr("");
-        requestify.post("http://" + req.get("host") + "/api/users/history")
-            .send({
-                "ticketID": ticketID
-            })
-            .then(function (resp) {
+        var ticketID = $(this).attr("tid");
+        var username = $("#username").attr("username");
 
-            });
+        $.ajax({
+            url: "/api/users/" + username + "/wishlist",
+            type: "POST",
+            data: {
+                "ticketID": ticketID
+            }
+        });
+
     });
 
 });
